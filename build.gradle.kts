c0cb553fa13fdb08a345ff50d09383912ae46a83
Leah Deshar <leah.desar@gmail.com>
2024-06-18 00:33:48 +0545

    Data binding image glide

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,6 +37,7 @@ android {
 dependencies {
     implementation("com.squareup.retrofit2:retrofit:2.11.0")
     implementation("com.squareup.retrofit2:converter-gson:2.11.0")
+    implementation("com.github.bumptech.glide:glide:4.16.0")
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
@@ -45,4 +46,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
+    annotationProcessor("com.github.bumptech.glide:compiler:4.16.0")
 }
