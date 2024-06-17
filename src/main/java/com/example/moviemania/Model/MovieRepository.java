23ca9aa10da2c67643683eae3fed35da125d139f
Leah Deshar <leah.desar@gmail.com>
2024-06-17 22:57:25 +0545

    Retrofit Call Enqueue

--- src/main/java/com/example/moviemania/Model/MovieRepository.java
+++ src/main/java/com/example/moviemania/Model/MovieRepository.java
@@ -45,7 +45,7 @@ public class MovieRepository {
              public void onFailure(Call<Result> call, Throwable t) {
 
              }
-            
+
          });
 
 
---
+++ src/main/java/com/example/moviemania/viewmodel/MainActivityViewModel.java
@@ -0,0 +1,26 @@
+package com.example.moviemania.viewmodel;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import com.example.moviemania.Model.Movie;
+import com.example.moviemania.Model.MovieRepository;
+
+import java.util.List;
+
+public class MainActivityViewModel extends AndroidViewModel {
+    private MovieRepository repository;
+    public MainActivityViewModel(@NonNull Application application) {
+        super(application);
+        this.repository = new MovieRepository(application);
+    }
+
+
+//    live data
+    public LiveData<List<Movie>> getAllMovies(){
+       return repository.getMutableLiveData();
+    }
+}
